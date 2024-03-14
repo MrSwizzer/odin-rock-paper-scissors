feee72113816a4e41fc5466d33ab1d306f8bda42
MrSwizzer <155651796+MrSwizzer@users.noreply.github.com>
2024-03-14 15:50:52 +0100

    Remove input check, since it's unnecessary for this demo

--- script.js
+++ script.js
@@ -9,75 +9,61 @@ function getComputerChoice(params) {
   }
 }
 
-function checkInput(input) {
-  return input === "rock" || input === "paper" || input === "scissors";
-}
-
 function playRound(computerSelection, playerSelcetion) {
-  if (checkInput(playerSelcetion)) {
-    if (playerSelcetion === computerSelection) {
-      return "It's a tie!";
-    } else if (playerSelcetion === "rock" && computerSelection === "scissors") {
-      return "You win!";
-    } else if (playerSelcetion === "rock" && computerSelection === "paper") {
-      return "Computer wins!";
-    } else if (
-      playerSelcetion === "scissors" &&
-      computerSelection === "paper"
-    ) {
-      return "You win!";
-    } else if (
-      playerSelcetion === "scissors" &&
-      computerSelection === "scissors"
-    ) {
-      return "Computer wins!";
-    } else if (playerSelcetion === "paper" && computerSelection === "rock") {
-      return "You win!";
-    } else if (
-      playerSelcetion === "paper" &&
-      computerSelection === "scissors"
-    ) {
-      return "Computer wins!";
-    }
-  } else {
-    return "Sorry, wrong input!";
+  if (playerSelcetion === computerSelection) {
+    return "It's a tie!";
+  } else if (playerSelcetion === "rock" && computerSelection === "scissors") {
+    return "You win!";
+  } else if (playerSelcetion === "rock" && computerSelection === "paper") {
+    return "Computer wins!";
+  } else if (playerSelcetion === "scissors" && computerSelection === "paper") {
+    return "You win!";
+  } else if (
+    playerSelcetion === "scissors" &&
+    computerSelection === "scissors"
+  ) {
+    return "Computer wins!";
+  } else if (playerSelcetion === "paper" && computerSelection === "rock") {
+    return "You win!";
+  } else if (playerSelcetion === "paper" && computerSelection === "scissors") {
+    return "Computer wins!";
   }
 }
 
-
- 
 function playGame() {
-    let playerCounter = 0;
-    let computerCounter = 0;
+  let playerCounter = 0;
+  let computerCounter = 0;
 
-    for (let index = 0; index < 5; index++) {
+  for (let index = 0; index < 5; index++) {
+    let computerChoice = getComputerChoice();
+    let playerChoice = prompt(
+      'Please choose between "Rock", "Paper" or "Scissors"'
+    );
+    playerChoice = playerChoice.toLowerCase();
 
-        let computerChoice = getComputerChoice();
-        let playerChoice = prompt('Please choose between "Rock" "Paper" or "Scissors"');
-        playerChoice = playerChoice.toLowerCase();
-        
-        console.log("You chose: " + playerChoice);
-        console.log("The computer chose: " + computerChoice);
+    console.log("You chose: " + playerChoice);
+    console.log("The computer chose: " + computerChoice);
 
-        let winner = playRound(computerChoice, playerChoice);
-        console.log(playRound(computerChoice, playerChoice));
-    
-        if (winner === "You win!") {
-            playerCounter++;
-        } else {
-            computerCounter++;
-        }
+    let winner = playRound(computerChoice, playerChoice);
+    console.log(playRound(computerChoice, playerChoice));
 
-        console.log("Player Counter: " + playerCounter);
-        console.log("Computer Counter: " + computerCounter);
-    }
-
-    if (playerCounter < computerCounter) {
-        return "Computer wins this game"
+    if (winner === "You win!") {
+      playerCounter++;
+    } else if (winner === "Computer wins!") {
+      computerCounter++;
     } else {
-        return "You won this game!"
+      console.log("It's a tie no points!");
     }
-    
+
+    console.log("Player Counter: " + playerCounter);
+    console.log("Computer Counter: " + computerCounter);
+  }
+
+  if (playerCounter < computerCounter) {
+    return "Computer wins this game";
+  } else {
+    return "You won this game!";
+  }
 }
 
 console.log(playGame());
